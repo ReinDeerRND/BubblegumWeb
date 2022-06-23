8984ba7009726f4b07747c742baa0e46e0ec2b34
Elena Swiderskaya <olenyonock7@rambler.ru>
2022-06-23 23:32:01 +0300

    redux-form

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,7 +1,37 @@
-const Login = ()=>{
+import { Field, reduxForm } from 'redux-form';
+import classes from './Login.module.css';
+
+const LoginForm = (props) => {
     return (
-        <div>
-            Login page 
+        <form onSubmit={props.handleSubmit}>
+            <div className={classes.field}>
+                <Field component="input" name = "login" placeholder="Login" />
+            </div>
+            <div className={classes.field}>
+                <Field component="input" name = "password"  placeholder="Password" />
+            </div>
+            <div className={classes.field}>
+                <Field component="input" name = "isRemember" type="checkbox" /> Remember me
+            </div>
+            <div className={classes.field}>
+                <button type="submit">Log In</button>
+            </div>
+        </form>
+    )
+}
+
+const LoginReduxForm = reduxForm({
+    form: 'login'
+})(LoginForm)
+
+const Login = () => {
+    const onSubmit = (formData)=>{
+        console.log(formData);
+    }
+    return (
+        <div className={classes.container}>
+            <h1>Login page</h1>
+            <LoginReduxForm onSubmit={onSubmit}/>
         </div>
     )
 }
--- src/redux/store.js
+++ src/redux/store.js
@@ -5,6 +5,7 @@ import friendsWidgetReducer from "./reducers/friendsWidgetReducer";
 import usersReducer from "./reducers/usersReducer";
 import authReducer from "./reducers/authReducer";
 import thunkMiddleware from "redux-thunk";
+import { reducer as formReducer } from 'redux-form'
 
 let reducers = combineReducers({
     profilePage: profileReducer,
@@ -12,6 +13,7 @@ let reducers = combineReducers({
     usersPage: usersReducer,
     friendsWidget: friendsWidgetReducer,
     auth: authReducer,
+    form: formReducer
 })
 
 let store = createStore(reducers, applyMiddleware(thunkMiddleware));
