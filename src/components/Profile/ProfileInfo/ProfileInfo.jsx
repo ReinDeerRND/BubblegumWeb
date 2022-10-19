6c8f4b7e7b4f9d5dd3f9303a7a5e86c543a15434
Elena Swiderskaya <olenyonock7@rambler.ru>
2022-10-19 22:39:14 +0300

    socials

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -30,7 +30,7 @@ const ProfileInfo = (props) => {
       <div className={classes.description_block}>
         <img alt="Avatar" src={props.profile.photos.small || userPhoto} />
         <div>
-          <div>{props.profile.fullName}</div>
+          <div>Name: {props.profile.fullName}</div>
           <div>About me: {props.profile.aboutMe}</div>
           <div>Search a Job: {props.profile.lookingForAJob ? <span>yes</span> : <span>no</span>}</div>
           <div>Searching Job Description:
@@ -40,20 +40,22 @@ const ProfileInfo = (props) => {
           </div>
           <div>Contacts: </div>
           <div className={classes.social_container}>
-            {props.profile.contacts.facebook ? <SocialItem social={props.profile.contacts.facebook} title="facebook" /> : null}
-            {props.profile.contacts.githab ? <SocialItem social={props.profile.contacts.githab} title="githab" /> : null}
-            {props.profile.contacts.instagram ? <SocialItem social={props.profile.contacts.instagram} title="instagram" /> : null}
-            {props.profile.contacts.mainLink ? <SocialItem social={props.profile.contacts.mainLink} title="mainLink" /> : null}
-            {props.profile.contacts.twitter ? <SocialItem social={props.profile.contacts.twitter} title="twitter" /> : null}
-            {props.profile.contacts.vk ? <SocialItem social={props.profile.contacts.vk} title="vk" /> : null}
-            {props.profile.contacts.website ? <SocialItem social={props.profile.contacts.website} title="website" /> : null}
-            {props.profile.contacts.youtube ? <SocialItem social={props.profile.contacts.youtube} title="youtube" /> : null}
+          {Object.keys(props.profile.contacts).map(key=>{
+            let value = props.profile.contacts[key];
+              if(value){
+                return <SocialItem social={value} title={key}/>
+              }
+              return null;
+            })}
           </div>
         </div>
       </div>
       <div className={classes.download_block}>
-        {props.isOwner && <input type="file" onChange={onFileSelected} />}
-        <button onClick={onUploadPhoto}>Upload Photo</button>
+        {props.isOwner && <div>
+          <input type="file" onChange={onFileSelected} />
+          <button onClick={onUploadPhoto}>Upload Photo</button>
+        </div>
+        }
       </div>
       <ProfileStatusWithHooks status={props.status} updateStatus={props.updateStatus} />
     </div>
